41807002bb958ca50980da5cd79ae6ac419e051f
huzhibin <2285901508@qq.com>
2023-04-29 14:45:13 +0800

    fead: add支持多个参数

--- src/number/add/index.ts
+++ src/number/add/index.ts
@@ -4,7 +4,7 @@
  * @param num2
  */
 
-const add = (num1: number, num2: number): number => {
+const summation = (num1: number, num2: number): number => {
   let r1; let r2;
   try {
     r1 = num1.toString().split('.')[1].length;
@@ -18,6 +18,11 @@ const add = (num1: number, num2: number): number => {
   }
   const m = 10 ** Math.max(r1, r2);
   return Math.round(num1 * m + num2 * m) / m;
+};
+
+const add = (...ages: number[]): number => {
+  const n = ages.reduce((per, cur) => summation(per, cur), 0);
+  return n;
 };
 
 export default add;
